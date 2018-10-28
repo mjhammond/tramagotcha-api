570dc46f389869974d9276bbed32993717d6d952
KafkaBob <bob.poole@laterooms.com>
2018-10-28 09:08:55 +0000

    fixed recursion

--- itemsForSale/index.js
+++ itemsForSale/index.js
@@ -6,20 +6,20 @@ module.exports = async (ID) =>{
     const Sqlresult = await database(`select * from itemsTable`);
     const users = await userDetails(ID);
     const result = Sqlresult.recordset;
+    var purchased = false;
     var items = [];
     for(i=0;i<result.length;i++){
         for(j=0;j<users.items.length;j++){
-            var purchased = false;
             if(users.items[j].itemId == result[i].ID){
                 purchased = true;
             }
-            items.push({
-                Id: result[i].ID,
-                Name: result[i].itemName,
-                Cost: 300,
-                purchased: purchased
-            });
         }
+        items.push({
+            Id: result[i].ID,
+            Name: result[i].itemName,
+            Cost: 300,
+            purchased: purchased
+        });
     }
     return items;
 }
