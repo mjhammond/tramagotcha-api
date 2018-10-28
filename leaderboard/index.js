b391016f41509281d2996a849fbb710debb39a0e
KafkaBob <bob.poole@laterooms.com>
2018-10-28 10:09:47 +0000

    added petId

--- leaderboard/index.js
+++ leaderboard/index.js
@@ -5,12 +5,12 @@ module.exports = async () =>{
     
     const result = sqlResult.recordset;
     var users = []
-    console.log(result)
     for(var i = 0; i < result.length;i++){
         const score = result[i].currentXp + ((result[i].currentLevel - 1) * 1000);
         users.push({
             username: result[i].Username,
-            score: score
+            score: score,
+            petId: result[i].PetId
         });
     };
     users.sort(function(a, b){
